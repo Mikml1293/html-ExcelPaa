d25d0c442c59e8c78c2849a068cf6789fbeb6057
Mikml1293 <123519907+Mikml1293@users.noreply.github.com>
2023-11-19 06:47:44 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by not editing{' '}
+          Get started by not evererrrr editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
